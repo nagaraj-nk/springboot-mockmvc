6f3fe4c23b87091a0356b9159adcc150c513c56e
nagaraj-nk <nnagarajece31@gmail.com>
2022-10-13 07:32:04 +0530

    INITIAL COMMIT

---
+++ src/main/java/com/srjons/test/SpringMvcMultiPartUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.srjons.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcMultiPartUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcMultiPartUnitTestApplication.class, args);
+	}
+
+}
